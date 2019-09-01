ce1e5a9280e7bc44a3114d0802edbcfa722d9760
Thamires Zamai <thamyzamai@gmail.com>
2019-09-01 20:21:12 -0300

    primeiro commit

---
+++ ControleTarefas/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ControleTarefas
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CarregarLista();
+        }
+
+        private void CarregarLista()
+        {
+            TarefasDBEntities context = new TarefasDBEntities();
+            List<TB_TAREFA> lstTarefa = context.TB_TAREFA.ToList<TB_TAREFA>();
+
+            GVTarefa.DataSource = lstTarefa;
+            GVTarefa.DataBind();
+        }
+    
+
+        public void DisplayAlert(string titulo, string mensagem, System.Web.UI.Page page)
+        {
+            h1.InnerText = titulo;
+            lblMsgPopup.InnerText = mensagem;
+            ClientScript.RegisterStartupScript(typeof(Page), Guid.NewGuid().ToString(), "MostrarPopupMensagem();", true);
+        }
+
+        protected void GVTarefa_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int idItem = Convert.ToInt32(e.CommandArgument.ToString());
+            TarefasDBEntities contextTarefa = new TarefasDBEntities();
+            TB_TAREFA tarefa = new TB_TAREFA();
+
+            tarefa = contextTarefa.TB_TAREFA.First(c => c.id == idItem);
+
+            if (e.CommandName == "ALTERAR")
+            {
+                Response.Redirect("Tarefa.aspx?idItem=" + idItem);
+            }
+            else if (e.CommandName == "EXCLUIR")
+            {
+                contextTarefa.TB_TAREFA.Remove(tarefa);
+                contextTarefa.SaveChanges();
+                string msg = "Tarefa excluida com sucesso!";
+                string titulo = "Informacao";
+                CarregarLista();
+                DisplayAlert(titulo, msg, this);
+            }
+        }
+    }
+}
---
+++ ControleTarefas/Tarefa.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ControleTarefas
+{
+    public partial class Tarefa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CarregarDadosPagina();
+            }
+        }
+
+        protected void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            string descricaoTarefa = txtDescricao.Text;
+            DateTime dtAtividade = Convert.ToDateTime(txtdtAtividade.Text);
+            DateTime dtEntrega = Convert.ToDateTime(txtdtEntrega.Text);
+            DateTime dtLimite = Convert.ToDateTime(txtdtLimite.Text);
+            TB_TAREFA t = new TB_TAREFA() { descricao = descricaoTarefa, dtAtividade = dtAtividade, dtEntrega = dtEntrega, dtLimiteEntrega = dtLimite };
+            TarefasDBEntities contextTarefa = new TarefasDBEntities();
+
+            string valor = Request.QueryString["idItem"];
+
+            if (String.IsNullOrEmpty(valor))
+            {
+                contextTarefa.TB_TAREFA.Add(t);
+                Diagnostico();
+                Clear();
+            }
+            else
+            {
+                int id = Convert.ToInt32(valor);
+                TB_TAREFA tarefa = contextTarefa.TB_TAREFA.First(c => c.id == id);
+                tarefa.descricao = t.descricao;
+                tarefa.dtAtividade = t.dtAtividade;
+                tarefa.dtEntrega = t.dtEntrega;
+                tarefa.dtLimiteEntrega = t.dtLimiteEntrega;
+                lblmsg.Text = "Registro Alterado!";
+            }
+
+            contextTarefa.SaveChanges();
+        }
+
+        private void CarregarDadosPagina()
+        {
+            string valor = Request.QueryString["idItem"];
+            int idItem = 0;
+            TB_TAREFA tarefa = new TB_TAREFA();
+            TarefasDBEntities contextTarefa = new TarefasDBEntities();
+
+            if (!String.IsNullOrEmpty(valor))
+            {
+                idItem = Convert.ToInt32(valor);
+                tarefa = contextTarefa.TB_TAREFA.First(c => c.id == idItem);
+
+                txtDescricao.Text = tarefa.descricao;
+                txtdtAtividade.Text = tarefa.dtAtividade.ToString();
+                txtdtEntrega.Text = tarefa.dtEntrega.ToString();
+                txtdtLimite.Text = tarefa.dtLimiteEntrega.ToString();
+
+            }
+        }
+
+        private void Clear()
+        {
+            txtdtAtividade.Text = "";
+            txtdtEntrega.Text = "";
+            txtdtLimite.Text = "";
+            txtDescricao.Text = "";
+            txtDescricao.Focus();
+        }
+
+        private void Diagnostico() {
+            
+            DateTime dtEntrega = Convert.ToDateTime(txtdtEntrega.Text);
+            DateTime dtLimite = Convert.ToDateTime(txtdtLimite.Text);
+
+            if (dtEntrega > dtLimite)
+            {
+                lblmsg.Text = "Preciso focar algumas horas da semana para poder entregar minhas atividades";
+            }
+            else if (dtEntrega == dtLimite)
+            {
+                lblmsg.Text = "Estou dentro do prazo";
+            }
+            else if (dtEntrega < dtLimite) {
+                lblmsg.Text = "Bruto!!";
+            }
+        }
+    }
+}
